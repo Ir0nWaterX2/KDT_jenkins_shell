96f6df879c8aa17498a5f1aa46d1cba2cf0e3704
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-26 11:09:47 +0900

    Update cronjob_adding.sh

--- cronjob_adding.sh
+++ cronjob_adding.sh
@@ -7,15 +7,15 @@ mkdir -p /home/ubuntu/crontab
 
 
 #  제외하고싶은 구문
-CRON_JOB="*/5 * * * * /home/ubuntu/crontab/resource_loging.sh"
+#CRON_JOB="*/5 * * * * /home/ubuntu/crontab/resource_loging.sh"
 
 
 
 # 현재 돌아가고있는 크론탭을 임시 파일에 백업
-crontab -l > /home/ubuntu/crontab/current_crontab.txt 2>/dev/null
+#crontab -l > /home/ubuntu/crontab/current_crontab.txt 2>/dev/null
 
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
-grep -v "$CRON_JOB" /home/ubuntu/crontab/current_crontab.txt > /home/ubuntu/crontab/new_crontab.txt
+#grep -v "$CRON_JOB" /home/ubuntu/crontab/current_crontab.txt > /home/ubuntu/crontab/new_crontab.txt
 
 
 #  로깅 쉘스크립트 등록 구문을 추가
@@ -29,7 +29,7 @@ crontab /home/ubuntu/crontab/new_crontab.txt
 
 
 # 임시 파일 삭제
-rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
+#rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
 
 # 크론 재실행 
 sudo systemctl restart cron
