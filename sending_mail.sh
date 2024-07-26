092c67bb63c7f1e059c6add61c865c2d95e7423c
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-26 17:43:21 +0900

    Update sending_mail.sh

--- sending_mail.sh
+++ sending_mail.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# PATH 환경 변수 설정
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+
 # 변수 설정
 RECIPIENT="a01084922552@gmail.com"
 SUBJECT="[김직원]로그파일 보내드립니다. "
@@ -11,4 +16,5 @@ FILENAME="/home/ubuntu/resource_usage.log"
 MUTT_PATH="/var/mail/root"
 
 # 이메일 전송
-echo "$BODY" | mutt -s "$SUBJECT" -a "$FILENAME" -- "$RECIPIENT"
+#echo "$BODY" | mutt -s "$SUBJECT" -a "$FILENAME" -- "$RECIPIENT"
+echo "$BODY" | $MUTT_PATH -s "$SUBJECT" -a "$FILENAME" -- "$RECIPIENT" > /home/ubuntu/shell/sending_mail.log 2>&1
