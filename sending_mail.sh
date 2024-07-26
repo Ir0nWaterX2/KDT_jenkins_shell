ee328ab4aab46ac3732af31ea4accdfd03b825f1
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-26 17:35:03 +0900

    Update sending_mail.sh

--- sending_mail.sh
+++ sending_mail.sh
@@ -6,5 +6,9 @@ SUBJECT="[김직원]로그파일 보내드립니다. "
 BODY="오늘자 로그입니다. 감사합니다.  "
 FILENAME="/home/ubuntu/resource_usage.log"
 
+
+
+MUTT_PATH="/var/mail/root"
+
 # 이메일 전송
 echo "$BODY" | mutt -s "$SUBJECT" -a "$FILENAME" -- "$RECIPIENT"
