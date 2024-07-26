4375f2692730ee406f38e57e638e9fce2605d3a1
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-26 15:45:21 +0900

    Update cronjob_adding.sh

--- cronjob_adding.sh
+++ cronjob_adding.sh
@@ -5,11 +5,16 @@
 # crontab 폴더 없으면 만들어주기 
 mkdir -p /home/ubuntu/crontab
 
+# 쉘스크립트에 권한주기 
+chmod +x /home/ubuntu/shell/resource_loging.sh
+chmod +x /home/ubuntu/shell/sending_mail.sh
 
-#  제외하고싶은 구문
+
+#  크론잡에 실행할 구문 
 CRON_JOB1="*/1 * * * * /home/ubuntu/shell/resource_loging.sh"
 #(실습용)
 CRON_JOB2="*/1 * * * * sudo logrotate -f /etc/logrotate.d/resourcelog"
+CRON_JOB3="*/2 * * * * /home/ubuntu/shell/sending_mail.sh"
 
 #chmod a+rw /home/ubuntu/crontab/new_crontab.txt
 #chmod a+rw /home/ubuntu/crontab/current_crontab.txt
@@ -19,7 +24,10 @@ crontab -l > /home/ubuntu/crontab/current_crontab.txt
 
 
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
-grep -v "* * * * * /home/ubuntu/shell/resource_loging.sh" /home/ubuntu/crontab/current_crontab.txt | grep -v "* * * * * sudo logrotate -f /etc/logrotate.d/resourcelog" > /home/ubuntu/crontab/new_crontab.txt
+grep -v "* * * * * /home/ubuntu/shell/resource_loging.sh" /home/ubuntu/crontab/current_crontab.txt | \
+grep -v "* * * * * sudo logrotate -f /etc/logrotate.d/resourcelog" | \
+grep -v "* * * * * /home/ubuntu/shell/sending_mail.sh" > /home/ubuntu/crontab/new_crontab.txt
+
 
 #  로깅 쉘스크립트 등록 구문을 추가
 echo "$CRON_JOB1" >> /home/ubuntu/crontab/new_crontab.txt
