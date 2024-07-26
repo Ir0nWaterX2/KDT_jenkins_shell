f8e62cc82f6fd01575f4a91543953a7a40a22987
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-26 14:38:12 +0900

    Update cronjob_adding.sh

--- cronjob_adding.sh
+++ cronjob_adding.sh
@@ -11,10 +11,13 @@ CRON_JOB1="*/1 * * * * /home/ubuntu/shell/resource_loging.sh"
 #(실습용)
 #CRON_JOB2="*/1 * * * * sudo logrotate -f /etc/logrotate.d/resourcelog"
 
-
+chmod a+rw /home/ubuntu/crontab/new_crontab.txt
+chmod a+rw /home/ubuntu/crontab/current_crontab.txt
 
 # 현재 돌아가고있는 크론탭을 임시 파일에 백업
 crontab -l > /home/ubuntu/crontab/current_crontab.txt
+
+
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
 grep -v "$CRON_JOB1" /home/ubuntu/crontab/current_crontab.txt > /home/ubuntu/crontab/new_crontab.txt
  
@@ -28,10 +31,6 @@ echo "$CRON_JOB1" >> /home/ubuntu/crontab/new_crontab.txt
 
 # 새로운 크론탭 파일을 적용
 crontab /home/ubuntu/crontab/new_crontab.txt
-
-
-# 임시 파일 삭제
-rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
 
 
 # old_log 폴더 없으면 만들어주기 
