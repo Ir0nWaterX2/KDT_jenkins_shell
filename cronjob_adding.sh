8db52cf33e90f40da1d321440d17f99dee7088b6
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-26 11:15:03 +0900

    Update cronjob_adding.sh

--- cronjob_adding.sh
+++ cronjob_adding.sh
@@ -7,7 +7,7 @@ mkdir -p /home/ubuntu/crontab
 
 
 #  제외하고싶은 구문
-#CRON_JOB="*/5 * * * * /home/ubuntu/crontab/resource_loging.sh"
+CRON_JOB="*/5 * * * * /home/ubuntu/crontab/resource_loging.sh"
 
 
 
@@ -15,11 +15,11 @@ mkdir -p /home/ubuntu/crontab
 #crontab -l > /home/ubuntu/crontab/current_crontab.txt 2>/dev/null
 
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
-#grep -v "$CRON_JOB" /home/ubuntu/crontab/current_crontab.txt > /home/ubuntu/crontab/new_crontab.txt
+grep -v "$CRON_JOB" /home/ubuntu/crontab/current_crontab.txt > /home/ubuntu/crontab/new_crontab.txt
 
 
 #  로깅 쉘스크립트 등록 구문을 추가
-echo "*/1 * * * * /home/ubuntu/shell/resource_loging.sh" >> /home/ubuntu/crontab/new_crontab.txt
+echo "*/1 * * * * /home/ubuntu/shell/resource_loging.sh" > /home/ubuntu/crontab/new_crontab.txt
 #실습용(로테이트를 1분에 한번씩 )
 echo "*/1 * * * * sudo logrotate -f /etc/logrotate.d/resourcelog" >> /home/ubuntu/crontab/new_crontab.txt
 
@@ -29,7 +29,7 @@ crontab /home/ubuntu/crontab/new_crontab.txt
 
 
 # 임시 파일 삭제
-#rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
+rm /home/ubuntu/crontab/current_crontab.txt /home/ubuntu/crontab/new_crontab.txt
 
 
 # old_log 폴더 없으면 만들어주기 
