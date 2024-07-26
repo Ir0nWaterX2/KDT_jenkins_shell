d3ef1701e47aa58a1c5324a0de1e00231f1efeeb
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-26 13:35:02 +0900

    Update cronjob_adding.sh

--- cronjob_adding.sh
+++ cronjob_adding.sh
@@ -15,10 +15,9 @@ CRON_JOB1="*/1 * * * * /home/ubuntu/shell/resource_loging.sh"
 
 # 현재 돌아가고있는 크론탭을 임시 파일에 백업
 crontab -l > /home/ubuntu/crontab/current_crontab.txt
-
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
 grep -v "$CRON_JOB1" /home/ubuntu/crontab/current_crontab.txt > /home/ubuntu/crontab/new_crontab.txt
-
+ 
 
 #  로깅 쉘스크립트 등록 구문을 추가
 echo "$CRON_JOB1" >> /home/ubuntu/crontab/new_crontab.txt
